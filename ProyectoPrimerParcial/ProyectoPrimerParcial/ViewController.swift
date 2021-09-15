b10a9a7d8c0e8638213f611c707631eb2f58b980
Paula-18 <53828123+Paula-18@users.noreply.github.com>
2021-09-14 17:35:54 -0700

    Add files via upload

---
+++ ProyectoPrimerParcial/ProyectoPrimerParcial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoPrimerParcial
+//
+//  Created by Alumno on 9/14/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
